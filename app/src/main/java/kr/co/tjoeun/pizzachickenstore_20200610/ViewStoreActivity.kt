5c26c686617378a248d335a359219dbaa31d71af
cho881020 <kj_cho@idealidea.co.kr>
2020-06-10 21:54:14 +0900

    가게 상세 정보 화면 XML 작업

---
+++ app/src/main/java/kr/co/tjoeun/pizzachickenstore_20200610/ViewStoreActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.pizzachickenstore_20200610
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
