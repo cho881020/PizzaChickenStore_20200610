45337fd7d2c0acb9b8a3f3f4440b737ac9634a27
cho881020 <kj_cho@idealidea.co.kr>
2020-06-10 20:12:56 +0900

    치킨가게 프래그먼트 생성

---
+++ app/src/main/java/kr/co/tjoeun/pizzachickenstore_20200610/fragments/ChickenStoreListFragment.kt
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.pizzachickenstore_20200610.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.co.tjoeun.pizzachickenstore_20200610.R
+
+class ChickenStoreListFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chicken_store_list, container, false)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
